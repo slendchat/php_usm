9a1a074facf4edea5329a23a1b0936e5bbd6ce2d
slendchat <slendchat@gmail.com>
2025-02-12 16:35:07 +0200

    lab01

---
+++ lab01/index.php
@@ -0,0 +1,15 @@
+<?php
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+// вывод конкатенацией
+echo $days . " дней. " . $message . "<br>";
+
+// вывод двойными ковычками
+echo "$days дней. $message<br>";
+
+echo "Привет, мир!";
+echo "Hello, World with echo!";
+print "Hello, World with print!";
+
+?>
