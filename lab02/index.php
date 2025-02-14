d3b9a8360ad5291d202a79a3dc1ff403437226b3
slendchat <slendchat@gmail.com>
2025-02-14 12:22:10 +0200

    lab02

---
+++ lab02/index.php
@@ -0,0 +1,112 @@
+<?php
+
+$dayofweek = date('N');
+
+if(in_array($dayofweek,[1,3,5]))
+{
+  $jhonsched = "8:00 - 12:00";
+}
+else
+{
+  $jhonsched = "Нерабочий день";
+}
+
+$janesched = (in_array($dayofweek, [2, 4, 6])) 
+    ? "12:00 - 16:00" 
+    : "Нерабочий день";
+
+?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Расписание</title>
+    <style>
+        table, td, th {
+            border: 1px solid #000;
+            border-collapse: collapse;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+<h2>Расписание на сегодня</h2>
+<table>
+    <tr>
+        <th>№</th>
+        <th>Фамилия Имя</th>
+        <th>График работы</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>John Styles</td>
+        <td><?php echo $jhonsched; ?></td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Jane Doe</td>
+        <td><?php echo $janesched; ?></td>
+    </tr>
+</table>
+<br><br><br>
+<?php
+echo "Loop made with: For<br>";
+$a = 0;
+$b = 0;
+
+for ($i = 0; $i <= 5; $i++) {
+  echo "i: ",$i,"<br>";
+  echo "a: ",$a,"<br>";
+  echo "b: ",$b,"<br>";
+   $a += 10;
+   $b += 5;
+   echo "<br>";
+}
+
+echo "End of the loop: a = $a, b = $b<br><br>";
+
+?>
+
+<?php
+echo "Loop made with: While<br>";
+$a = 0;
+$b = 0;
+$i = 0;
+
+while($i<=5) {
+  echo "i: ",$i,"<br>";
+  echo "a: ",$a,"<br>";
+  echo "b: ",$b,"<br>";
+   $a += 10;
+   $b += 5;
+   echo "<br>";
+   $i++;
+}
+
+echo "End of the loop: a = $a, b = $b<br><br>";
+
+?>
+
+<?php
+echo "Loop made with: Do-While<br>";
+$a = 0;
+$b = 0;
+$i = 0;
+
+ do{
+  echo "i: ",$i,"<br>";
+  echo "a: ",$a,"<br>";
+  echo "b: ",$b,"<br>";
+   $a += 10;
+   $b += 5;
+   echo "<br>";
+   $i++;
+}while($i<=5);
+
+echo "End of the loop: a = $a, b = $b<br><br>";
+
+?>
+</body>
+</html>
+
